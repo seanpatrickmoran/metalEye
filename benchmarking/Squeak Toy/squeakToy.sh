9b400a2616b23b2fa3dc2734a4b6c27a760e38b5
seanpatrickmoran <seanpatrickmoran@gmail.com>
2025-02-27 16:34:48 -0500

    benchmark

---
+++ benchmarking/Squeak Toy/squeakToy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /User/sean/git/MLX/metalEye/benchmarking/Squeak\ Toy
+python A1_llama3.2_squeakToy.py
+python C2_llama3.2_squeakToy.py
+python A2_llama3.2_squeakToy.py
+python C3_llama3.2_squeakToy.py
+python A3_llama3.2_squeakToy.py
+python D1_llama3.2_squeakToy.py
+python B1_llama3.2_squeakToy.py
+python D2_llama3.2_squeakToy.py
+python B2_llama3.2_squeakToy.py
+python D3_llama3.2_squeakToy.py
+python B3_llama3.2_squeakToy.py
+python C1_llama3.2_squeakToy.py
+
