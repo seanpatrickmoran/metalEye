0bb505af50b52a1781f3fa265be48be767a23b2f
seanpatrickmoran <seanpatrickmoran@gmail.com>
2025-02-28 10:38:06 -0500

    test table virtualization

--- benchmarking/formVirtTables.sh
+++ benchmarking/formVirtTables.sh
@@ -2,29 +2,29 @@
 
 # python ../utils/sqlitevec2sqlite3fts5.py /Users/seanmoran/Documents/Master/2025/Feb2025/virtualTabless/EB_databaseVEC_18.db  /Users/seanmoran/Documents/Master/2025/Feb2025/LariatTables/virtualTables/test.db
 python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/A1_llama3.2-3B-khImage+hist.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/A1_llama3.2-3B-fts5_khImage+hist.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/A2_llama3.2-3B-khImage.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/A2_llama3.2-3B-fts5_khImage.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/A3_llama3.2-3B-hist.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/A3_llama3.2-3B-fts5_hist.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/B1_llama3.2-3B-khImage+epi-1bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/B1_llama3.2-3B-fts5_khImage+epi-1bin.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/B2_llama3.2-3B-khImage+epi-5bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/B2_llama3.2-3B-fts5_khImage+epi-5bin.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/B3_llama3.2-3B-khImage+epi-11bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/B3_llama3.2-3B-fts5_khImage+epi-11bin.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/C1_llama3.2-3B-hist+epi-1bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/C1_llama3.2-3B-fts5_hist+epi-1bin.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/C2_llama3.2-3B-hist+epi-5bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/C2_llama3.2-3B-fts5_hist+epi-5bin.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/C3_llama3.2-3B-hist+epi-11bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/C3_llama3.2-3B-fts5_hist+epi-11bin.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/D1_llama3.2-3B-khImage+hist+epi-1bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/D1_llama3.2-3B-fts5_khImage+hist+epi-1bin.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/D2_llama3.2-3B-khImage+hist+epi-5bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/D2_llama3.2-3B-fts5_khImage+hist+epi-5bin.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/D3_llama3.2-3B-khImage+hist+epi-11bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/D3_llama3.2-3B-fts5_khImage+hist+epi-11bin.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy2/ebTable/E1_llama3.2-3B-Image+hist.db /Users/sean/Documents/Master/2025/March2025/SqueakToy2/virtualTables/E1_llama3.2-3B-fts5_Image+hist.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy2/ebTable/E2_llama3.2-3B-Image.db /Users/sean/Documents/Master/2025/March2025/SqueakToy2/virtualTables/E2_llama3.2-3B-fts5_Image.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy2/ebTable/F1_llama3.2-3B-Image+epi-1bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy2/virtualTables/F1_llama3.2-3B-fts5_Image+epi-1bin.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy2/ebTable/F2_llama3.2-3B-Image+epi-5bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy2/virtualTables/F2_llama3.2-3B-fts5_Image+epi-5bin.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/F3_llama3.2-3B-Image+epi-11bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/F3_llama3.2-3B-fts5_Image+epi-11bin.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/A2_llama3.2-3B-khImage.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/A2_llama3.2-3B-fts5_khImage.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/A3_llama3.2-3B-hist.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/A3_llama3.2-3B-fts5_hist.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/B1_llama3.2-3B-khImage+epi-1bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/B1_llama3.2-3B-fts5_khImage+epi-1bin.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/B2_llama3.2-3B-khImage+epi-5bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/B2_llama3.2-3B-fts5_khImage+epi-5bin.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/B3_llama3.2-3B-khImage+epi-11bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/B3_llama3.2-3B-fts5_khImage+epi-11bin.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/C1_llama3.2-3B-hist+epi-1bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/C1_llama3.2-3B-fts5_hist+epi-1bin.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/C2_llama3.2-3B-hist+epi-5bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/C2_llama3.2-3B-fts5_hist+epi-5bin.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/C3_llama3.2-3B-hist+epi-11bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/C3_llama3.2-3B-fts5_hist+epi-11bin.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/D1_llama3.2-3B-khImage+hist+epi-1bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/D1_llama3.2-3B-fts5_khImage+hist+epi-1bin.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/D2_llama3.2-3B-khImage+hist+epi-5bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/D2_llama3.2-3B-fts5_khImage+hist+epi-5bin.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/D3_llama3.2-3B-khImage+hist+epi-11bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/D3_llama3.2-3B-fts5_khImage+hist+epi-11bin.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/E1_llama3.2-3B-Image+hist.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/E1_llama3.2-3B-fts5_Image+hist.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/E2_llama3.2-3B-Image.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/E2_llama3.2-3B-fts5_Image.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/F1_llama3.2-3B-Image+epi-1bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/F1_llama3.2-3B-fts5_Image+epi-1bin.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/F2_llama3.2-3B-Image+epi-5bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/F2_llama3.2-3B-fts5_Image+epi-5bin.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/ebTable/F3_llama3.2-3B-Image+epi-11bin.db /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/F3_llama3.2-3B-fts5_Image+epi-11bin.db
 
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/ebTable/A1_miniCPMv26_image.db /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/A1_miniCPMv26_fts5_image.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/ebTable/A2_miniCPMv26_KHimage.db /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/A2_miniCPMv26_fts5KHimage.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/ebTable/B1_miniCPMv26_Image+epi-1bin.db /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/B1_miniCPMv26_fts5_Image+epi-1bin.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/ebTable/B2_miniCPMv26_Image+epi-5bin.db /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/B2_miniCPMv26_fts5_Image+epi-5bin.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/ebTable/B3_miniCPMv26_Image+epi-11bin.db /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/B3_miniCPMv26_fts5_Image+epi-11bin.db
-python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/ebTable/C1_miniCPMv26_Image+epi-11bin.db /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/C1_miniCPMv26_fts5_Image+epi-11bin+motif.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/ebTable/A1_miniCPMv26_image.db /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/A1_miniCPMv26_fts5_image.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/ebTable/A2_miniCPMv26_KHimage.db /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/A2_miniCPMv26_fts5KHimage.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/ebTable/B1_miniCPMv26_Image+epi-1bin.db /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/B1_miniCPMv26_fts5_Image+epi-1bin.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/ebTable/B2_miniCPMv26_Image+epi-5bin.db /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/B2_miniCPMv26_fts5_Image+epi-5bin.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/ebTable/B3_miniCPMv26_Image+epi-11bin.db /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/B3_miniCPMv26_fts5_Image+epi-11bin.db
+# python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/ebTable/C1_miniCPMv26_Image+epi-11bin.db /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/C1_miniCPMv26_fts5_Image+epi-11bin+motif.db
 
 
 ######### D2 has typo. Change when model run is done and change in the virt table form. OK
@@ -41,29 +41,29 @@ python ../utils/sqlitevec2sqlite3fts5.py /Users/sean/Documents/Master/2025/March
 
 #this should not be necessary to do more than once?
 python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/A1_llama3.2-3B-fts5_khImage+hist.db 3072
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/A2_llama3.2-3B-fts5_khImage.db 3072
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/A3_llama3.2-3B-fts5_hist.db 3072
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/B1_llama3.2-3B-fts5_khImage+epi-1bin.db 3072
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/B2_llama3.2-3B-fts5_khImage+epi-5bin.db 3072
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/B3_llama3.2-3B-fts5_khImage+epi-11bin.db 3072
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/C1_llama3.2-3B-fts5_hist+epi-1bin.db 3072
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/C2_llama3.2-3B-fts5_hist+epi-5bin.db 3072
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/C3_llama3.2-3B-fts5_hist+epi-11bin.db 3072
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/D1_llama3.2-3B-fts5_khImage+hist+epi-1bin.db 3072
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/D2_llama3.2-3B-fts5_khImage+hist+epi-5bin.db
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/D3_llama3.2-3B-fts5_khImage+hist+epi-11bin.db
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy2/virtualTables/E1_llama3.2-3B-fts5_Image+hist.db 3072
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/E2_llama3.2-3B-fts5_Image.db 3072
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy2/virtualTables/F1_llama3.2-3B-fts5_Image+epi-1bin.db 3072
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy2/virtualTables/F2_llama3.2-3B-fts5_Image+epi-5bin.db 3072
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/F3_llama3.2-3B-fts5_Image+epi-11bin.db 3072
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/A2_llama3.2-3B-fts5_khImage.db 3072
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/A3_llama3.2-3B-fts5_hist.db 3072
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/B1_llama3.2-3B-fts5_khImage+epi-1bin.db 3072
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/B2_llama3.2-3B-fts5_khImage+epi-5bin.db 3072
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/B3_llama3.2-3B-fts5_khImage+epi-11bin.db 3072
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/C1_llama3.2-3B-fts5_hist+epi-1bin.db 3072
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/C2_llama3.2-3B-fts5_hist+epi-5bin.db 3072
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/C3_llama3.2-3B-fts5_hist+epi-11bin.db 3072
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/D1_llama3.2-3B-fts5_khImage+hist+epi-1bin.db 3072
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/D2_llama3.2-3B-fts5_khImage+hist+epi-5bin.db
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/D3_llama3.2-3B-fts5_khImage+hist+epi-11bin.db
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/E1_llama3.2-3B-fts5_Image+hist.db 3072
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/E2_llama3.2-3B-fts5_Image.db 3072
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/F1_llama3.2-3B-fts5_Image+epi-1bin.db 3072
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/F2_llama3.2-3B-fts5_Image+epi-5bin.db 3072
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/SqueakToy/virtualTables/F3_llama3.2-3B-fts5_Image+epi-11bin.db 3072
 
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/A1_miniCPMv26_fts5_image.db 3584
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/A2_miniCPMv26_fts5KHimage.db 3584
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/B1_miniCPMv26_fts5_Image+epi-1bin.db 3584
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/B2_miniCPMv26_fts5_Image+epi-5bin.db 3584
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/B3_miniCPMv26_fts5_Image+epi-11bin.db 3584
-python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/C1_miniCPMv26_fts5_Image+epi-11bin+motif.db 3584
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/A1_miniCPMv26_fts5_image.db 3584
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/A2_miniCPMv26_fts5KHimage.db 3584
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/B1_miniCPMv26_fts5_Image+epi-1bin.db 3584
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/B2_miniCPMv26_fts5_Image+epi-5bin.db 3584
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/B3_miniCPMv26_fts5_Image+epi-11bin.db 3584
+# python ../utils/writeFaissIndices.py /Users/sean/Documents/Master/2025/March2025/Tomahawk/virtualTables/C1_miniCPMv26_fts5_Image+epi-11bin+motif.db 3584
 
 
 # python ../utils/sqlitevec2sqlite3fts5.py /Users/seanmoran/Documents/Master/2025/Feb2025/virtualTabless/EB_databaseVEC_18.db  /Users/seanmoran/Documents/Master/2025/Feb2025/LariatTables/virtualTables/test.db
